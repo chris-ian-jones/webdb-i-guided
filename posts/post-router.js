764a61b6d7625e307ddf260fb311300d8f0a001c
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-09 19:52:49 +0100

    WIP: initial commit, updated router endpoints

--- posts/post-router.js
+++ posts/post-router.js
@@ -6,23 +6,72 @@ const db = require('../data/db-config.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+  db.select('*')
+    .from('posts')
+    .then(posts => {
+      res.status(200).json(posts)
+    }).catch(err => {
+      res.json(err)
+    })
 });
 
 router.get('/:id', (req, res) => {
+  const { id: id } = req.params
 
+  db('posts')
+    .where({ id })
+    .first()
+    .then(posts => {
+      res.status(200).json(posts)
+    }).catch(err => {
+      res.json(err)
+    })
 });
 
 router.post('/', (req, res) => {
+  const postData = req.body
 
+  db('posts')
+    .insert(postData, 'id')
+    .then(([id]) => {
+      db('posts')
+      .where({ id })
+      .first()
+      .then(post => {
+        res.status(200).json(post)
+      })
+      .catch(err => {
+        res.status(404).json(err)
+      })
+    }).catch(err => {
+      res.json(err)
+    })
 });
 
 router.put('/:id', (req, res) => {
+  const changes = req.body
 
+  db('posts')
+    .where('id', req.params.id)
+    .update(changes)
+    .then(count => {
+      res.status(200).json({message: `updated ${count} records`})
+    })
+    .catch(err => {
+      res.status(err)
+    })
 });
 
 router.delete('/:id', (req, res) => {
-
+  db('posts')
+    .where({id: req.params.id})
+    .del()
+    .then(count => {
+      res.status(200).json({message: `deleted ${count} records`})
+    })
+    .catch(err => {
+      res.status(err)
+    })
 });
 
 module.exports = router;
